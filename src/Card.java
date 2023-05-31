346cb717f0347cded0962e0804b6dc28a60ad77a
KundanKumar61 <kumkunda@sheridancollege.ca>
2023-05-31 13:57:55 -0400

    ICE1

---
+++ src/Card.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author kunda
+ */
+class Card {
+
+    static String[] SUITS;
+
+    void setValue(int value) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    void setSuit(String suit) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static class SUITS {
+
+        public SUITS() {
+        }
+    }
+    
+}
